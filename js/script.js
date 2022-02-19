51f3ce85929c7483fa7400cf452f0f403771ed8c
Aviral Khanduja <83770372+aviralkhanduja@users.noreply.github.com>
2022-02-19 19:59:55 +0530

    Smooth Scroll and skill bars animation added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var tags=document.querySelectorAll('nav li a[href*="#"]');
-console.log(tags);
+//console.log(tags);
 for(var i=0;i<tags.length;i++){
     tags[i].addEventListener('click',function(event){
         event.preventDefault();
@@ -18,3 +18,44 @@ for(var i=0;i<tags.length;i++){
         }
     });
 }
+var skill_levels=document.querySelectorAll('.skill-container div');
+var check_filling=[];
+for(var i=0;i<skill_levels.length;i++)
+{
+    check_filling.push(false);
+}
+window.addEventListener('scroll',checkscroll);
+function checkscroll()
+{
+    for(let i=0;i<skill_levels.length;i++)
+    {
+        let coordinates=skill_levels[i].getBoundingClientRect();
+        //console.log(coordinates);
+        if(!check_filling[i]&&coordinates.top<=window.innerHeight&&coordinates.top>=-40)
+        {
+            fillbar(skill_levels[i]);
+            check_filling[i]=true;
+        }
+        if(coordinates.top<-40||coordinates.top>window.innerHeight)
+        {
+            check_filling[i]=false;   
+        }
+    }   
+}
+function fillbar(skill_level)
+{
+    let value=skill_level.getAttribute('data-width');
+    let currentWidth=0;
+    const id=setInterval(function(){
+        //console.log(skill_levels[i].style.width);
+        if(currentWidth>=value)
+        {
+            clearInterval(id);
+            return;
+        }
+        currentWidth++;
+        skill_level.style.width=currentWidth+"%";
+        //console.log(skill_levels[i].style.width);
+    },4);
+}
+
